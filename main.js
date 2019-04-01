0e907d397ec0e4b93c2001fb509762ebecd8462f
Andres Burget <aburget@MBP-ABURGET.local>
2019-04-01 10:57:35 +0300

    add challenge

--- main.js
+++ main.js
@@ -8,6 +8,13 @@ app.get('/', function (req,res) {
   };
   res.json(reply);
 });
+app.post('/action-endpoint', function (req, res) {
+  const challenge = req.body.challenge;
+  const reply = {
+    "challenge": challenge
+  };
+  res.json(reply);
+});
 app.use(bodyParser.json());
 const listener = app.listen(process.env.PORT || '3000', function () {
   console.log('Your app is listening on port ' + listener.address().port);
