e55fbee8c212f164acd8d271557817903f879463
Andres Burget <aburget@MBP-ABURGET.local>
2019-04-01 10:29:42 +0300

    ad healthcheck

---
+++ main.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const request = require('request');
+app.get('/', function (req,res) {
+  const reply = {
+    "status": "ok"
+  };
+  res.json(reply);
+});
+app.use(bodyParser.json());
+const listener = app.listen(process.env.PORT || '3000', function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+git add .
+git commit -am 'empty program'
+git push
